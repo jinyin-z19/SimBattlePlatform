3398ceb06d8f1558d5edd23ac7f3b98abe5db5c7
jinyin-z19 <19807733522@163.com>
2023-07-28 19:34:48 +0800

    update main controller

--- battle_platform/scripts/battle_platform_controller.py
+++ battle_platform/scripts/battle_platform_controller.py
@@ -1,29 +1,25 @@
 #!/usr/bin/env python
 
 import math
+import time
 import os
 import rospy
 import argparse
 import actionlib
+import cv2
+import numpy 
 
 from geometry_msgs.msg import Twist, Vector3, Quaternion
 from sensor_msgs.msg import JointState, Image, Imu
 from bitbots_msgs.msg import JointCommand, KickGoal, KickAction, KickFeedback
-
+from tf.transformations import quaternion_from_euler
+from cv_bridge import CvBridge, CvBridgeError
 
 class robot_kick:
     def __init__(self, token):
         self.kick_goal = KickGoal()
         self.kick_client = actionlib.SimpleActionClient(token + '/dynamic_kick', KickAction)
         self.kick_client.wait_for_server()
-    def done_cb(self, state, result):
-        rospy.loginfo("kick action complete")
-
-    def active_cb(self):
-        rospy.loginfo('kick server accepted')
-
-    def feedback_cb(self, feedback):
-        rospy.loginfo(feedback)   
       
     def kick(self, kick_pos, kick_speed, wait_time_before_kick):
         self.kick_goal.header.seq = 1
@@ -44,22 +40,19 @@ class robot_kick:
 
         rospy.sleep(wait_time_before_kick)
 
-        kick_client.send_goal(self.kick_goal)
+        self.kick_client.send_goal(self.kick_goal)
         rospy.loginfo("send kick")
 
-        self.kick_client.done_cb = done_cb
-        #self.kick_client.feedback_cb = feedback_cb
-        self.kick_client.active_cb = active_cb
         self.kick_client.wait_for_result()
         
         rospy.loginfo("kick down")   
     
 class battle_player:
     def __init__(self, bot_side, bot_index):
-        self.token = self.side + str(self.index)
+        self.token = bot_side + str(bot_index)
 
-        self.walk_goal_publisher = rospy.Publisher(self.token + '/cmd_vel', JointState, queue_size=1)
-        self.head_goal_publisher = rospy.Publisher(self.token + '/walking_motor_goals', JointState, queue_size=1)
+        self.walk_goal_publisher = rospy.Publisher(self.token + '/cmd_vel', Twist, queue_size=1)
+        self.head_goal_publisher = rospy.Publisher(self.token + '/walking_motor_goals', JointCommand, queue_size=1)
         self.imu_subscriber = rospy.Subscriber(self.token + '/imu/data_raw', Imu, self.imu_callback, queue_size=1, tcp_nodelay=True)
         self.img_subscriber = rospy.Subscriber(self.token + '/usb_cam/image_raw', Image, self.img_callback, queue_size=1, tcp_nodelay=True)
         
@@ -67,25 +60,24 @@ class battle_player:
         
         self.head_goal_msg = JointCommand()
         self.head_goal_msg.joint_names = ["neck", "head"]
-        self.head_goal_msg.velocities = [np.pi, np.pi]
+        self.head_goal_msg.velocities = [3.1415926, 3.1415926]
         
         self.walk_goal_msg = Twist()
+        
+        self.image_raw = Image()
         
         self.accl_info = [0,0,0]
         self.gyro_info = [0,0,0]
     
-    def bot_token(self)
+    def bot_token(self):
         return self.token
     
     def img_callback(self, img_raw):
-        return img_raw
+        self.image_raw = img_raw
 
     def imu_callback(self, imu_raw):
-        accelerometer_info = [imu_raw.linear_acceleration.x, imu_raw.linear_acceleration.y, imu_raw.linear_acceleration.z] 
-        self.accl_info =  accelerometer_info      
-        gyroscope_info = [imu_raw.angular_velocity.x, imu_raw.angular_velocity.y, imu_raw.angular_velocity.z] 
-        self.gyro_info =  gyroscope_info 
-        return accelerometer_info, gyroscope_info
+        self.accl_info = [imu_raw.linear_acceleration.x, imu_raw.linear_acceleration.y, imu_raw.linear_acceleration.z]     
+        self.gyro_info = [imu_raw.angular_velocity.x, imu_raw.angular_velocity.y, imu_raw.angular_velocity.z] 
         
     def pub_walk_goal(self, walk_goal_sim):
         '''
@@ -110,7 +102,18 @@ class battle_player:
         wait_time_before_kick (double)
         '''
         self.bot_kick.kick(kick_pos, kick_speed, wait_time_before_kick) 
-         
+
+    def get_latest_imu(self): 
+        return self.accl_info, self.gyro_info
+
+    def get_latest_img(self):
+        bridge = CvBridge() 
+        image = bridge.imgmsg_to_cv2(self.image_raw, "bgr8")
+        image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
+        return image  
+
+#-----------example-------------
+     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--bot_num', type = int, default = None)
@@ -122,29 +125,48 @@ if __name__ == "__main__":
     red_bots = []
     blue_bots = []
     token_list = []
-    if(bot_num > 0):
+    if(args.bot_num > 0):
         for i in range(args.bot_num):
-            red_bots.append( battle_player('r', i))
-            blue_bots.append(battle_player('b', i))
-            token_list.append( self.red_bots[i].bot_token())
-            token_list.append(self.blue_bots[i].bot_token())
+            red_bots.append( battle_player('r', i + 1))
+            blue_bots.append(battle_player('b', i + 1))
+            token_list.append( red_bots[i].bot_token())
+            token_list.append(blue_bots[i].bot_token())
     else:
         pass
         
     print(token_list)
     
     while not rospy.is_shutdown():
+
         # walk example
         red_bots[0].pub_walk_goal([0.1,0,0])
         time.sleep(3)
+
         red_bots[0].pub_walk_goal([0,0,0])
         time.sleep(0.5)
+
         # head example
         red_bots[0].pub_head_goal([0.7,0.7])
         time.sleep(3)
+
         red_bots[0].pub_head_goal([0,0])
+        time.sleep(0.5)
+
+        # read photo example
+        img_r1 = red_bots[0].get_latest_img()
+        cv2.imshow('img3', img_r1)
+        cv2.waitKey(0)
+
+        # read imu example
+        accl_r1, gyro_r1 = red_bots[0].get_latest_imu()
+        print("accelerometer")
+        print(accl_r1)
+        print("gyroscope")
+        print(gyro_r1)
+
         # kick example
         red_bots[0].pub_kick_goal([0.3,0.05,0])
+
         
 
         
